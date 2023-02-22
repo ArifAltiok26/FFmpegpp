f9fbedb5b8744daa4ea11a2bf6cb1450fbc2130d
ArifAltiok26 <a.altiok26@gmail.com>
2023-02-22 10:00:12 +0000

    added from and open policies tags into outputformat_context

--- src/policies/output_format_context.cpp
+++ src/policies/output_format_context.cpp
@@ -11,4 +11,14 @@ int OutputFormatContext_w::execute(AVFormatContext *context, const Write &, AVPa
     return av_interleaved_write_frame(context, packet);
 }
 
+int OutputFormatContext_w::execute(AVFormatContext *context, const From &, AVDictionary **options)
+{
+    return avformat_write_header(context, options);
+}
+
+int OutputFormatContext_w::execute(AVFormatContext *context, const Open &, const char *filename)
+{
+    return avio_open(&context->pb, filename, AVIO_FLAG_WRITE);
+}
+
 } // namespace ygv::policies
--- src/policies/output_format_context.h
+++ src/policies/output_format_context.h
@@ -1,11 +1,19 @@
 #pragma once
 struct AVFormatContext;
 struct AVPacket;
+struct AVDictionary;
+
+#include "tags/from.h"
+#include "tags/open.h"
 #include "tags/write.h"
 namespace ygv::policies
 {
 struct OutputFormatContext_w
 {
     static int execute(AVFormatContext *context, const Write &, AVPacket *packet);
+
+    static int execute(AVFormatContext *context, const From &, AVDictionary **options);
+
+    static int execute(AVFormatContext *context, const Open &, const char *filename);
 };
 } // namespace ygv::policies
