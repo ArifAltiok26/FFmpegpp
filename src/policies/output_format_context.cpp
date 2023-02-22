2202e50332c779206d60d5d144eaa9399643a7b6
ArifAltiok26 <a.altiok26@gmail.com>
2023-02-22 12:54:53 +0000

    added create tag into output_format_context policies

--- src/policies/output_format_context.cpp
+++ src/policies/output_format_context.cpp
@@ -21,4 +21,8 @@ int OutputFormatContext_w::execute(AVFormatContext *context, const Open &, const
     return avio_open(&context->pb, filename, AVIO_FLAG_WRITE);
 }
 
+AVStream *OutputFormatContext_w::execute(AVFormatContext *context, const Create &, const AVCodec *codec)
+{
+    return avformat_new_stream(context, codec);
+}
 } // namespace ygv::policies
--- src/policies/output_format_context.h
+++ src/policies/output_format_context.h
@@ -2,7 +2,10 @@
 struct AVFormatContext;
 struct AVPacket;
 struct AVDictionary;
+struct AVStream;
+struct AVCodec;
 
+#include "tags/create.h"
 #include "tags/from.h"
 #include "tags/open.h"
 #include "tags/write.h"
@@ -15,5 +18,7 @@ struct OutputFormatContext_w
     static int execute(AVFormatContext *context, const From &, AVDictionary **options);
 
     static int execute(AVFormatContext *context, const Open &, const char *filename);
+
+    static AVStream *execute(AVFormatContext *context, const Create &, const AVCodec *codec = nullptr);
 };
 } // namespace ygv::policies
