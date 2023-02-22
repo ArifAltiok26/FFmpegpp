69334fe88d6fe56b142ff9825e90c5154600305e
ArifAltiok26 <a.altiok26@gmail.com>
2023-02-22 10:18:22 +0000

    Audio fifo allocators functions are made static

--- src/allocators/avaudio_fifo.h
+++ src/allocators/avaudio_fifo.h
@@ -7,11 +7,11 @@ namespace ygv::allocators
 
 struct AVAudioFifo_w
 {
-    AVAudioFifo *allocate();
+    static AVAudioFifo *allocate();
 
-    AVAudioFifo *allocate(int sample_fmt, int channels, int nb_samples = 0);
+    static AVAudioFifo *allocate(int sample_fmt, int channels, int nb_samples = 0);
 
-    void destroy(AVAudioFifo *buff);
+    static void destroy(AVAudioFifo *buff);
 };
 
 } // namespace ygv::allocators
